12a2e20619f366b60706b7e5c6268afdbf90fd05
jonathan <jonathan.wirz@icloud.com>
2025-03-21 20:57:14 +0100

    added .env

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = "8bd4fbc98ef565a9eb2b93540b25bf9e";
+const apiKey = process.env.API_KEY;
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const airQualityApiUrl = "http://api.openweathermap.org/data/2.5/air_pollution";
 
@@ -164,6 +164,76 @@ async function updateWeatherAutomatically() {
   const lat = await checkCordslat(defaultCity);
   const lon = await checkCordslon(defaultCity);
   await checkAirQuality(lat, lon);
+}
+async function checkWeatherByCoords(lat, lon) {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`)
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".Weather").style.display = "none";
+  } else {
+    var data = await response.json();
+    console.log(data);
+
+    document.querySelector(".search").placeholder = data.name;
+    const weatherDescription = data.weather[0].description;
+    document.querySelector(".weather-description").innerHTML = weatherDescription.charAt(0).toUpperCase() + weatherDescription.slice(1);
+    document.querySelector(".location").innerHTML = data.name + ", " + data.sys.country;
+
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+    const formattedDate = `${day}.${month}.${year}`;
+    document.querySelector(".date").innerHTML = formattedDate;
+
+    document.querySelector(".temperature").innerHTML =
+        Math.round(data.main.temp) + "°C";
+    document.querySelector("#sunset").innerHTML = convertUnixToAMPM(data.sys.sunset);
+    document.querySelector("#first-metric-sunrise-sunset").innerHTML = convertUnixToAMPM(data.sys.sunrise);
+    document.querySelector("#visibility").innerHTML = (data.main.pressure /1000).toFixed(2) + '<span class="units">km</span>';
+    document.querySelector("#pressure").innerHTML = data.main.pressure + '<span class="units">hPa</span>';
+    document.querySelector("#humidity").innerHTML = data.main.humidity + '<span class="units">%</span>';
+    document.querySelector("#feels-like").innerHTML = Math.round(data.main.feels_like) + '<span class="units">°C</span>';
+
+    if (data.weather[0].icon == "01d") {
+      weatherIcon.src = "images/type=01d.svg";
+    } else if (data.weather[0].icon == "02d") {
+      weatherIcon.src = "images/type=02d.png";
+    } else if (data.weather[0].icon === "03d") {
+      weatherIcon.src = "images/type=03d.svg";
+    } else if (data.weather[0].icon === "04d") {
+      weatherIcon.src = "images/type=04d.svg";
+    } else if (data.weather[0].icon === "09d") {
+      weatherIcon.src = "images/type=09d.svg";
+    }else if (data.weather[0].icon == "10d") {
+      weatherIcon.src = "images/type=10d.svg";
+    }else if (data.weather[0].icon == "11d") {
+      weatherIcon.src = "images/type=11d.svg";
+    }else if (data.weather[0].icon == "13d") {
+      weatherIcon.src = "images/type=13d.svg";
+    }else if (data.weather[0].icon == "50d") {
+      weatherIcon.src = "images/type=50d.svg";
+    }else if(data.weather[0].icon == "01n") {
+      weatherIcon.src = "images/type=01n.svg";
+    }else if (data.weather[0].icon == "02n") {
+      weatherIcon.src = "images/type=02n.svg";
+    } else if (data.weather[0].icon == "03n") {
+      weatherIcon.src = "images/type=03n.svg";
+    } else if (data.weather[0].icon == "04n") {
+      weatherIcon.src = "images/type=04n.svg";
+    } else if (data.weather[0].icon == "09n") {
+      weatherIcon.src = "images/type=09n.svg";
+    }else if (data.weather[0].icon == "10n") {
+      weatherIcon.src = "images/type=10n.svg"}
+    else if (data.weather[0].icon == "11n") {
+      weatherIcon.src = "images/type=11n.svg"}
+    else if (data.weather[0].icon == "13n") {
+      weatherIcon.src = "images/type=13n.svg"}
+    else if (data.weather[0].icon == "50n") {
+      weatherIcon.src = "images/type=50n.svg"}
+  }
+  checkAirQuality(lat, lon);
 }
 
 updateWeatherAutomatically();
